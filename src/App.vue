1e46015c269f82de20060062c83722b05dea5c6b
LuizCarlosdeSouzaAlmeida <luizmk_rulez@live.com>
2022-02-05 17:59:08 -0300

    Create project and setup axios

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div v-for="n of news.news" :key="n.id">{{ n.category }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    news() {
+      return this.$store.state.news;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getNews");
+  },
+};
+</script>
+
+<style>
+div {
+  color: red;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+const REQUEST_URL = "https://mocki.io/v1/e7550604-190d-4d41-8097-4383cf9dfaca";
+export default createStore({
+  state: {
+    news: [],
+    selectedNew: {
+      id: null,
+      date: null,
+      category: "teste",
+      url_image: "",
+      title: "",
+      short_text: "",
+      long_text: "",
+    },
+  },
+  getters: {},
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    },
+  },
+  actions: {
+    getNews({ commit }) {
+      axios.get(REQUEST_URL).then((response) => {
+        commit("SET_NEWS", response.data);
+      });
+    },
+  },
+  modules: {},
+});
